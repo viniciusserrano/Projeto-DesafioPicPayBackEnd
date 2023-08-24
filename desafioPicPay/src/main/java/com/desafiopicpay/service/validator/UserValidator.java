68f159ba20b8382b60e47f66bbde289d13c8daf0
Vinicius Serrano <viniciusgomesserrano@gmail.com>
2023-08-24 15:52:48 -0300

    Ajustes de Refatoração na validator

--- desafioPicPay/src/main/java/com/desafiopicpay/service/validator/UserValidator.java
+++ desafioPicPay/src/main/java/com/desafiopicpay/service/validator/UserValidator.java
@@ -2,9 +2,13 @@ package com.desafiopicpay.service.validator;
 
 import com.desafiopicpay.repository.entity.User;
 import com.desafiopicpay.repository.enums.UserTypeEnum;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
 
+@Configuration
+@ComponentScan("com.desafiopicpay.service.validator")
 public class UserValidator {
 
 
